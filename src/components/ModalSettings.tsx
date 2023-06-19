6cdf4715d8a851f84e3afb7a2e8cc8527e091e07
felixesteban100 <felixesteban100@gmail.com>
2023-06-18 22:12:20 -0400

    new version

--- src/components/ModalSettings.tsx
+++ src/components/ModalSettings.tsx
@@ -271,7 +271,7 @@ function ModalSettings({
                                     <button
                                         // onClick={() => setVersionsDownloaded([])}
                                         onClick={async () => {
-                                            await dbForVersionsDownloaded.clear("versionsDownloaded")
+                                            // await dbForVersionsDownloaded.clear("versionsDownloaded")
                                         }}
                                     >
                                         <svg className="fill-primary w-8 h-10 hover:text-red-500" /* className="fill-current hover:text-red-500" width="40" height="40" */ viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path className="fill-current hover:text-red-500" fill="#000000" d="M352 192V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64H96a32 32 0 0 1 0-64h256zm64 0h192v-64H416v64zM192 960a32 32 0 0 1-32-32V256h704v672a32 32 0 0 1-32 32H192zm224-192a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32zm192 0a32 32 0 0 0 32-32V416a32 32 0 0 0-64 0v320a32 32 0 0 0 32 32z" /></svg>
